6f49756819c45a20e860d15390d72e41a9d26c54
unifinetsol <vasant84kumar@gmail.com>
2018-11-26 13:55:01 +0530

    final Source

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// Side Nav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+})
